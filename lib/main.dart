7bd2096d4c791b80c814d9ce39095eb04a83ac5c
FlutLab.io <>
2022-01-04 06:41:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class HomePage extends StatelessWidget {
           decoration: BoxDecoration(
               color: Colors.teal,
               gradient: LinearGradient(colors: [
-                Colors.red,
+                Colors.yellow,
                 Colors.pink,
               ])), //BoxDecoration
           child: Text("I am a Box"),
