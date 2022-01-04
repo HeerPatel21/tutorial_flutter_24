728a629f11d0fcacfc6c22a3ba289cfd97d71189
FlutLab.io <>
2022-01-04 06:34:04 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(8),
-          alignment: 
           color: Colors.purple,
           width: 100,
           height: 100,
