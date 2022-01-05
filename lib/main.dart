ccc5dc80c14c697579c050bfd42f62239bc46b25
FlutLab.io <>
2022-01-05 06:33:40 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,9 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ), //AppBar
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Align(
-          alignment: Alignment.bottomRight,
-          child: Container(
-            color: Colors.black,
-            width: 200,
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                //
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ), //Container
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ), //container
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ), //container
-                ], //<Widget>
-              ),
-            ), //row
-          ),
-        ),
-      ), //container
-    ); //scaffold
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ), //AppBar
+        body: Container()); //scaffold
   }
 }
