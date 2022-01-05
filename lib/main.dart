278ecbbb2f4c06212b2c7348a52f401777b24521
FlutLab.io <>
2022-01-05 06:19:03 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,30 +14,34 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ), //AppBar
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.green,
-          ), //Container
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.yellow,
-          ), //container
-          Container(
-            padding: const EdgeInsets.all(8),
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ), //container
-        ], //<Widget>
-      ), //row
-    ); //Scaffold
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          //
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ), //Container
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ), //container
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
+            ), //container
+          ], //<Widget>
+        ), //row
+      ), //container
+    ); //scaffold
   }
 }
