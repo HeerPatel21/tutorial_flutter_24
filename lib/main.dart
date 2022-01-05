e24876a3a0b8a24f2eefd400f1343c0350aea2a4
FlutLab.io <>
2022-01-05 06:28:05 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,40 @@ class HomePage extends StatelessWidget {
       ), //AppBar
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Container(
-          color: Colors.black,
-          width: 200,
-          height: 400,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              //
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  width: 100,
-                  height: 100,
-                  color: Colors.green,
-                ), //Container
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  width: 100,
-                  height: 100,
-                  color: Colors.yellow,
-                ), //container
-                Container(
-                  padding: const EdgeInsets.all(8),
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                ), //container
-              ], //<Widget>
-            ),
-          ), //row
+        child: Align(
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                //
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ), //Container
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ), //container
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ), //container
+                ], //<Widget>
+              ),
+            ), //row
+          ),
         ),
       ), //container
     ); //scaffold
