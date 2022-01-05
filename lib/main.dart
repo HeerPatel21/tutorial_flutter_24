d94ceaaeeee629b553ebd4ab0d228c3dcf018ee4
FlutLab.io <>
2022-01-05 06:26:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -20,30 +20,33 @@ class HomePage extends StatelessWidget {
           color: Colors.black,
           width: MediaQuery.of(context).size.width,
           height: MediaQuery.of(context).size.height,
-          child: Column(
-            //
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              ), //Container
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ), //container
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ), //container
-            ], //<Widget>
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              //
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                ), //Container
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ), //container
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ), //container
+              ], //<Widget>
+            ),
           ), //row
         ),
       ), //container
