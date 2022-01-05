0c61fa3ccfa934cefb4de822d17d34ccd09ae6a3
FlutLab.io <>
2022-01-05 05:58:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,9 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(8),
-          alignment: Alignment.center,
           //color: Colors.purple,//commented as we need to give color inside decoration property
           width: 100,
           height: 100,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[400],
-                    blurRadius: 5,
-                    //spreadRadius: 10,
-                    offset: Offset(2.0, 5.0)) //BoxShadow
-              ],
-              color: Colors.teal,
-              gradient: LinearGradient(colors: [
-                Colors.yellow,
-                Colors.pink,
-              ])), //BoxDecoration
-          child: Text(
-            "I am a Box",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
         ), //Container
       ), //Center
     ); //Scaffold
