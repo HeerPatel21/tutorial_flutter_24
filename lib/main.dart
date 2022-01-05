4b3980cdcb47b0bbb8c6e2d0c2c3edd36d9aa493
FlutLab.io <>
2022-01-05 06:41:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.edit),
+        hoverColor: Colors.green,
       ),
     ); //scaffold
   }
