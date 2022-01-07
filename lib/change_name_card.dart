1ffd344c430d6bfd29b3496c7caf5ecfa902b3ab
FlutLab.io <>
2022-01-07 06:26:22 +0000

    Autosave

--- lib/change_name_card.dart
+++ lib/change_name_card.dart
@@ -3,30 +3,7 @@ import 'package:flutter/material.dart';
 class ChangeNameCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-              children: <Widget>[
-                BgImage(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ), //text
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _nameController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter Something Here",
-                      labelText: "Name",
-                    ),
-                  ),
-                ),
-              ], //widget
-            ), //column
+    return 
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,30 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Card(
-            child: ChangeNameCard(),
+            child: Column(
+              children: <Widget>[
+                BgImage(),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ), //text
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    controller: _nameController,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Something Here",
+                      labelText: "Name",
+                    ),
+                  ),
+                ),
+              ], //widget
+            ), //column
           ), //card
         ), //SingleChildScrollView
       ), //padding
