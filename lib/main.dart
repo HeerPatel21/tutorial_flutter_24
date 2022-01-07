1335e058846590d08ba74cfd1e926faf60600ca4
FlutLab.io <>
2022-01-07 06:20:35 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -38,30 +38,7 @@ class _HomePageState extends State<HomePage> {
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
           child: Card(
-            child: Column(
-              children: <Widget>[
-                BgImage(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  myText,
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ), //text
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: TextField(
-                    controller: _nameController,
-                    keyboardType: TextInputType.text,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Enter Something Here",
-                      labelText: "Name",
-                    ),
-                  ),
-                ),
-              ], //widget
-            ), //column
+            child: ChangeNameCard(),
           ), //card
         ), //SingleChildScrollView
       ), //padding
