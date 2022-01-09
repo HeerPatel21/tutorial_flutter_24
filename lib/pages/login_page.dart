40d0cfde127c876df8a771c7ae7a1dfae6f1e334
FlutLab.io <>
2022-01-09 06:05:47 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final userNameController = TextEditingController
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar : AppBar(
+        title:Text("Login Page"),
+      ),//AppBar
+      body:SingleChildScrollView(
+        child:Column(),//Column
+      ),//singlechildScrollView
+
+    );//Scaffold
   }
 }
