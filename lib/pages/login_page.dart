e2f183160bd8c6da4f258cb641768065cd7b9805
FlutLab.io <>
2022-01-10 05:10:57 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,28 +20,25 @@ class _LoginPageState extends State<LoginPage> {
           children: <Widget>[
             BgImage(),
             Padding(
-              padding:const EdgeInsets.all(8.0),
-              child:SingleChildScrollView(
-                child:Card(
-                  child:Column(
-                    children:<Widget>[
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
                       TextFormField(
-                        decoration:InputDecoration(
-                          hintText:"Enter Username",
-                          labelText:"Username",
-                        ),//inputDecoration
-                      )//TextFormField
+                        decoration: InputDecoration(
+                          hintText: "Enter Username",
+                          labelText: "Username",
+                        ), //inputDecoration
+                      ), //TextFormField
                       SizedBox(
-                        height:20,
+                        height: 20,
                       ),
                       TextFormField(
-                        decoration:InputDecoration(
-                          hintText:"Enter Password",
-                          labelText:"Password"
-                        ),//InputDecoration
-                      )//TextFormField
-                    ],//widget
-                  ),//column
+                        decoration: InputDecoration(hintText: "Enter Password", labelText: "Password"), //InputDecoration
+                      ) //TextFormField
+                    ], //widget
+                  ), //column
                 ),
               ),
             ),
