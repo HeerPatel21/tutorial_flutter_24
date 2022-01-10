3b03a5f9132861c7c1575abf7f112a8ddd9dd8d3
FlutLab.io <>
2022-01-10 06:05:22 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -12,46 +12,13 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ), //AppBar
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: SingleChildScrollView(
-          child: Card(
-            child: Column(
-              children: <Widget>[
-                Form(
-                    child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(hintText: "Enter Username", labelText: "Username"),
-                      ), //Textformfield
-                      SizedBox(
-                        height: 20,
-                      ), //sizedbox
-                      TextFormField(
-                        decoration: InputDecoration(hintText: "Enter Password", labelText: "Password"),
-                      ), //Textformfield
-                    ], //widget
-                  ), //column
-                )), //padding//form
-                SizedBox(
-                  height: 20,
-                ), //sizedbox
-                RaisedButton(
-                  onPressed: () {},
-                  child: Text("Sign In"),
-                  color: Colors.purple,
-                  textColor: Colors.white,
-                ) //raisedButton
-              ], //widget
-            ), //column
-          ), //card
-        ), //single child
-      ), //pading
-    ); //Scaffold
+        appBar: AppBar(
+          title: Text("Login Page"),
+        ), //AppBar
+        body: Stack(
+          children: <Widget>[
+            BgImage(),
+          ], //widget
+        )); //stack//Scaffold
   }
 }
