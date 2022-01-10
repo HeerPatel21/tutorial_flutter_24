009f5c646ea688f80c48dbef541dfa7deec37271
FlutLab.io <>
2022-01-10 06:06:48 +0000

    Autosave

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,6 +19,46 @@ class _LoginPageState extends State<LoginPage> {
           fit: StackFit.expand,
           children: <Widget>[
             BgImage(),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SingleChildScrollView(
+                child: Card(
+                  child: Column(
+                    children: <Widget>[
+                      Form(
+                          child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextFormField(
+                              decoration: InputDecoration(hintText: "Enter Username", labelText: "Username"),
+                            ), //Textformfield
+                            SizedBox(
+                              height: 20,
+                            ), //sizedbox
+                            TextFormField(
+                              decoration: InputDecoration(hintText: "Enter Password", labelText: "Password"),
+                            ), //Textformfield
+                          ], //widget
+                        ), //column
+                      )), //padding//form
+                      SizedBox(
+                        height: 20,
+                      ), //sizedbox
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Text("Sign In"),
+                          color: Colors.purple,
+                          textColor: Colors.white,
+                        ), //button
+                      ) //padding
+                    ], //widget
+                  ), //column
+                ), //card
+              ), //single child
+            ), //pading
           ], //widget
         )); //stack//Scaffold
   }
