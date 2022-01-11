0c8adacf50ce09297936f4d924459de2327e8378
FlutLab.io <>
2022-01-11 05:48:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -10,12 +9,12 @@ void main() {
         home: LoginPage(),
         theme: ThemeData(
           primarySwatch: Colors.purple,
-        ),
-        routes: {
-      "/login": (context) => LoginPage(),
-      "/home": (context) => HomePage(),
-      ),
-    
-    },
-  ); //MaterialApp
+        )
+        routes:{
+          "/login":(context)=>LoginPage(),
+          "/home":(context)=>HomePage(),
+        },
+      ),//MaterialApp
+        
+  ); 
 }
