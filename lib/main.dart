ccffe1f475fc7cc40224b3d9c19dd3c44af3caae
FlutLab.io <>
2022-01-11 06:02:56 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 
-void main() {
+Future main() async {
+  WidgetFlutterBinding.ensureInitialized();
+
+  Constants.prefs = await SharedPrefrences.getInstance();
   runApp(
     MaterialApp(
       title: "Awesome App",
