3ec83bb965639033b499511cfaff3ac3b8e913b9
FlutLab.io <>
2022-01-11 05:41:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,9 @@ void main() {
         theme: ThemeData(
           primarySwatch: Colors.purple,
         )),
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/home": (context) => HomePage(),
+    },
   ); //MaterialApp
 }
